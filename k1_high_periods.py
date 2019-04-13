6ae0542ad9623f67ae05b8c9a157b0dd10ddc90c
Michael Hippke <hippke@users.noreply.github.com>
2019-04-13 19:13:46 +0200

    Update k1_high_periods.py

--- k1_high_periods.py
+++ k1_high_periods.py
@@ -378,8 +378,9 @@ def get_planets(KIC):
 def seach_one_koi(KOI):
 
     # Check if vetting file exists already
-    figure_out_path = '_FAIL50-1500d_' + str(KOI) + '_0'
-    if figure_out_path + '_0' in " ".join(glob.glob("*.png")):
+    figure_out_path = 'FAIL50-1500d_' + str(KOI)
+    figure_out_path2 = 'GOOD50-1500d_' + str(KOI)
+    if figure_out_path in " ".join(glob.glob("*.png")) or figure_out_path2 in " ".join(glob.glob("*.png")):
         print('Vetting sheet for this KOI exists already, skipping KOI', KOI)
     else:
         print('Working on file', KOI)
